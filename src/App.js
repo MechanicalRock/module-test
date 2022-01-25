f3366f3be1aa27d7ba002a39684909a6e95a8b77
Tim Veletta <tim.veletta@mechanicalrock.io>
2022-01-25 13:39:40 +0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, Typography } from '@mui/material';
+import theme from 'tim-veletta-theme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Typography variant="h1" color="primary">
+				This should be in the primary color #40bfb4
+			</Typography>
+			<Typography variant="h1" color="secondary">
+				This should be in the secondary color #e15554
+			</Typography>
+		</ThemeProvider>
+	);
 }
 
 export default App;
